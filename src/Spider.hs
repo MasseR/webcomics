86fa0194ed8b5b62dc485a6d6196dc4fe9493b3d
Mats Rauhala <mats.rauhala@iki.fi>
2017-10-07 22:54:30 +0300

    Spider with retry
    Change-Id: I720d1ce94b43976629889564840b926a30cebe0c

--- src/Spider.hs
+++ src/Spider.hs
@@ -1,3 +1,5 @@
+{-# Language FlexibleContexts #-}
+{-# Language TemplateHaskell #-}
 module Spider where
 
 import Network.Wreq.Session
@@ -9,6 +11,11 @@ import Control.Monad.Reader
 import Text.XML (Document)
 import Text.HTML.DOM (parseLBS)
 import Lens.Micro
+import Control.Exception.Lifted
+import Control.Monad.Trans.Control
+import Control.Concurrent.Lifted (threadDelay)
+import qualified Data.Text as T
+import Lenses
 
 class HasSession a where
     getSession :: a -> Session
@@ -16,13 +23,29 @@ class HasSession a where
 instance HasSession Session where
     getSession = id
 
+catchAll :: (MonadBaseControl IO m) => m a -> (SomeException -> m a) -> m a
+catchAll = catch
+
+retry :: (MonadLogger m, MonadBaseControl IO m) => Int -> m a -> m a
+retry 0 f = error "exhausted retries"
+retry n f = doRetry baseDelay 0
+    where
+        baseDelay = 1 * (10 ^ 6)
+        doRetry delay r | r >= n = error "exhausted retries"
+                        | otherwise = catchAll f (caught r (delay + (r * (10 ^ 6))))
+        caught r delay e = do
+            $(logWarn) (T.pack $ show e)
+            $(logWarn) (T.pack $ "Delaying for " ++ (show (delay `div` (10 ^ 6))) ++ " seconds")
+            threadDelay delay
+            doRetry delay (r+1)
+
 fetchDocument :: (HasSession r, MonadReader r m, MonadLogger m, MonadIO m) => String -> m Document
 fetchDocument url = do
     session <- asks getSession
     r <- liftIO $ get session url
     return $ parseLBS (r ^. responseBody)
 
-spiderPage :: (HasSession r, MonadReader r m, MonadLogger m, MonadIO m) => Rule -> String -> m Page
+spiderPage :: (HasSession r, MonadReader r m, MonadLogger m, MonadIO m, MonadBaseControl IO m) => Rule -> String -> m Page
 spiderPage (Rule c parser) url = do
-    doc <- fetchDocument url
+    doc <- retry 5 (fetchDocument url)
     return $ parser c doc
